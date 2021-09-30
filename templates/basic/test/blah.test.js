1a708519cbc58c68f5419cbd892245769a86bd1b
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2021-09-30 23:00:57 +0530

    feat: use templates and mustache to generate scaffold

---
+++ templates/basic/test/blah.test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const src_1 = require("../src");
+describe('blah', () => {
+    it('works', () => {
+        expect((0, src_1.sum)(1, 1)).toEqual(2);
+    });
+});
